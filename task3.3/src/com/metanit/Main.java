d6b5a68135ff3f71ffa005a8b47af73ebe8a4513
Dmitry <dmitry.bichikov@gmail.com>
2019-09-17 15:20:16 +0300

    add

---
+++ task3.3/src/com/metanit/Main.java
@@ -0,0 +1,21 @@
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char[] str={'H','e','l','l','o',' ','W','o','r','d',' ','2','0','1','9'};
+        System.out.println("Исходная строка");
+        System.out.println(str);
+        numberDigits(str);
+    }
+
+    private static void numberDigits(char[] line){
+        int number=0;
+        for (char i:line){
+            if(Character.isDigit(i)){
+                number++;
+            }
+        }
+        System.out.println("В строке "+number+" числа(ел)!");
+    }
+}
